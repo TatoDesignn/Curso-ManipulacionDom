5a797c734ab64bc54d7a1cdb219de2acb0eb7021
TATO DESIGN <73967293+TatoDesign@users.noreply.github.com>
2023-02-23 22:17:22 -0500

    Primera Subida

---
+++ Practicas/Leer.js
@@ -0,0 +1,9 @@
+document.getElementById('EL NOMBRE DEL ID'); // sin el #
+document.getElementsByTagName('EL NOMBRE DE LA ETIQUETA');
+document.getElementsByClassName('EL NOMBRE DE LA CLASE'); //sin el .
+
+document.querySelector('#Miidie');
+document.querySelector('.Miclase');  //solo nos trae el primer elemento con esa clase o idie
+
+document.querySelectorAll('#miIdie');
+document.querySelectorAll('.Miclase') //traemos todos los elementos con esa clase o idie
