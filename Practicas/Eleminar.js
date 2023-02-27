76a7b3701180a41c09e1e6ae9fbf466f977a6259
TATO DESIGN <73967293+TatoDesign@users.noreply.github.com>
2023-02-26 22:51:02 -0500

    Actualizacion 2

---
+++ Practicas/Eleminar.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.Padre');
+const hijo = document.querySelector('.hijo');
+
+container.removeChild(hijo);
+
+const container2 = hijo.parentElement;
+
+container.remove
+
+container.replaceChild(variable1, remplazo);
